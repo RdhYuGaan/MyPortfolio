7b3bf346b1b9bd88dd7a7dc28d48d1b77ac5b657
RdhYuGaan <radithyugan22@gmail.com>
2025-01-04 00:15:43 +0530

    add some photos

--- app/utils/dataBase.js
+++ app/utils/dataBase.js
@@ -5,7 +5,7 @@ export const navLinks = [
   { id: 'about-Me', label: 'About Me' },
   { id: 'services', label: 'Services' },
   { id: 'projects', label: 'Projects' },
-  { id: 'testimonials', label: 'Testimonials' },
+  // { id: 'testimonials', label: 'Testimonials' },
   { id: 'contact', label: 'Contact' },
 ];
 
@@ -58,73 +58,73 @@ export const projects = [
   },
   {
     imgSrc: '/web/web-img-5.png',
-    altText: 'Landing Page Design',
-    title: 'Landing Page Design',
+    altText: ' Pet Care and Monitering Mobile Application',
+    title: ' Pet Care and Monitering Mobile Application',
     category: 'UI/UX',
   },
-  {
-    imgSrc: '/web/web-img-6.png',
-    altText: 'Social Media Dashboard',
-    title: 'Social Media Dashboard',
-    category: 'Graphic Design',
-  },
+  // {
+  //   imgSrc: '/web/web-img-6.png',
+  //   altText: 'Social Media Dashboard',
+  //   title: 'Social Media Dashboard',
+  //   category: 'Graphic Design',
+  // },
 ];
 
 
 export const services = [
   {
-    imgSrc: '/ui.png',
+    imgSrc: '/UI.png',
     altText: 'UI/UX Design',
     title: 'UI/UX Design',
     description:
       'Crafting intuitive and beautiful user interfaces that enhance user experiences and streamline interactions.',
   },
   {
-    imgSrc: '/web-design.png',
-    altText: 'Web Design',
-    title: 'Web Design',
+    imgSrc: '/web.png',
+    altText: 'Web  Design',
+    title: 'Web Application Design',
     description:
       'Designing responsive and visually stunning websites that provide seamless navigation across all devices.',
   },
   {
-    imgSrc: '/app-design.png',
+    imgSrc: '/app.png',
     altText: 'App Design',
-    title: 'App Design',
+    title: 'Mobile Application Design',
     description:
       'Creating sleek and functional mobile app designs that offer a delightful user experience on both iOS and Android.',
   },
-  {
-    imgSrc: '/graphic-design.png',
-    altText: 'Graphic Design',
-    title: 'Graphic Design',
-    description:
-      'Delivering creative graphic design solutions, from branding to marketing materials, that make a lasting impact.',
-  },
+  // {
+  //   imgSrc: '/graphic-design.png',
+  //   altText: 'Graphic Design',
+  //   title: 'Graphic Design',
+  //   description:
+  //     'Delivering creative graphic design solutions, from branding to marketing materials, that make a lasting impact.',
+  // },
 ];
 
-export const testimonials = [
-  {
-    name: 'John Doe',
-    review:
-      'Working with you was an absolute pleasure. Your attention to detail and creativity made the process smooth and enjoyable. I am thrilled with the final product!',
-    imgSrc: '/profile/profile-1.jpg',
-  },
-  {
-    name: 'Jane Smith',
-    review:
-      'The design exceeded my expectations! You understood my needs perfectly and delivered a stunning website that truly reflects my brand.',
-    imgSrc: '/profile/profile-2.jpg',
-  },
-  {
-    name: 'Michael Lee',
-    review:
-      'Your professionalism and dedication to the project were outstanding. I appreciate how you went above and beyond to make sure everything was perfect!',
-    imgSrc: '/profile/profile-3.jpg',
-  },
-  {
-    name: 'Emily Davis',
-    review:
-      'I was blown away by your ability to bring my vision to life. The end result is beautiful and exactly what I was hoping for. Thank you!',
-    imgSrc: '/profile/profile-4.jpg',
-  },
-]; 
+// export const testimonials = [
+//   {
+//     name: 'John Doe',
+//     review:
+//       'Working with you was an absolute pleasure. Your attention to detail and creativity made the process smooth and enjoyable. I am thrilled with the final product!',
+//     imgSrc: '/profile/profile-1.jpg',
+//   },
+//   {
+//     name: 'Jane Smith',
+//     review:
+//       'The design exceeded my expectations! You understood my needs perfectly and delivered a stunning website that truly reflects my brand.',
+//     imgSrc: '/profile/profile-2.jpg',
+//   },
+//   {
+//     name: 'Michael Lee',
+//     review:
+//       'Your professionalism and dedication to the project were outstanding. I appreciate how you went above and beyond to make sure everything was perfect!',
+//     imgSrc: '/profile/profile-3.jpg',
+//   },
+//   {
+//     name: 'Emily Davis',
+//     review:
+//       'I was blown away by your ability to bring my vision to life. The end result is beautiful and exactly what I was hoping for. Thank you!',
+//     imgSrc: '/profile/profile-4.jpg',
+//   },
+// ]; 
