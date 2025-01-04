f856a38fed4724f95001ca17ef3a168b017b9044
RdhYuGaan <radithyugan22@gmail.com>
2025-01-04 23:15:18 +0530

    commenting testimonialcard

--- app/sections/TestimonialsSection.jsx
+++ app/sections/TestimonialsSection.jsx
@@ -1,31 +1,31 @@
-import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
-import { testimonials } from "../utils/dataBase";
+// import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+// import { testimonials } from "../utils/dataBase";
 
-function TestimonialsSection() {
-  return (
-    <section 
-        id="testimonials"
-        className="my-[3rem] w-full md:my-[6rem] overflow-x-clip">
+// function TestimonialsSection() {
+//   return (
+//     <section 
+//         id="testimonials"
+//         className="my-[3rem] w-full md:my-[6rem] overflow-x-clip">
     
-         <h2 className="mb-4 text-3xl font-bold text-center lg:mb-8">
-               Testimonials
-         </h2>
+//          <h2 className="mb-4 text-3xl font-bold text-center lg:mb-8">
+//                Testimonials
+//          </h2>
 
-            <p className="mb-4 lg:mb-8 text-center mx-auto max-w-[36rem] text-black/60">
-                Hear what my clients have to say about their experience and the value
-                I brought to their projects through creativity and collaboration.
-            </p>
+//             <p className="mb-4 lg:mb-8 text-center mx-auto max-w-[36rem] text-black/60">
+//                 Hear what my clients have to say about their experience and the value
+//                 I brought to their projects through creativity and collaboration.
+//             </p>
 
-            <div className="w-screen">
-                <InfiniteMovingCards
-                     items={testimonials}
-                     direction="left"
-                     speed="normal"
-                />     
-            </div>
+//             <div className="w-screen">
+//                 <InfiniteMovingCards
+//                      items={testimonials}
+//                      direction="left"
+//                      speed="normal"
+//                 />     
+//             </div>
 
-    </section>
-  );
-}
+//     </section>
+//   );
+// }
 
-export default TestimonialsSection;
+// export default TestimonialsSection;
